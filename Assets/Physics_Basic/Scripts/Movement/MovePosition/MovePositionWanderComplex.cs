8e6a87fdafeeb1750eda2ef56a9f261b8d16d37c
Owen Mundy <omundy@gmail.com>
2020-09-24 10:59:18 -0400

    Adds complex wandering with collision detection

--- Assets/Physics_Basic/Scripts/Movement/MovePosition/MovePositionWanderComplex.cs
+++ Assets/Physics_Basic/Scripts/Movement/MovePosition/MovePositionWanderComplex.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovePositionWander : PhysicsBase {
+public class MovePositionWanderComplex : PhysicsBase {
 
 	public Vector3 direction; // a new direction vector
 
@@ -12,6 +12,7 @@ public class MovePositionWander : PhysicsBase {
 	public Vector3 wayPoint;            // new position to head towards
 	public float targetThreshold = 1f;  // test distance to target - must be > 0
 	public float pointSelectRange = 6f; // range from which to select new wayPoint
+
 
 
 	private void Start ()
@@ -31,8 +32,8 @@ public class MovePositionWander : PhysicsBase {
 			wayPoint = ReturnNewWanderPoint ();
 		}
 
-		// look at wayPoint
-		transform.LookAt (wayPoint);
+		// slowly rotate towards the wayPoint
+		RotateTowardsTargetOverTime (wayPoint, 1f, 3f);
 
 		//Debug.Log (wayPoint + " and " + (transform.position - wayPoint).magnitude);
 
@@ -64,7 +65,7 @@ public class MovePositionWander : PhysicsBase {
 				1,
 				Random.Range (transform.position.z - pointSelectRange, transform.position.z + pointSelectRange)
 			);
-			//Debug.Log ("🙌 target is within collider = " + IsPointWithinCollider (worldContainerCollider, target));
+			//Debug.Log("🙌 target is within collider = " + IsPointWithinCollider(worldContainerCollider, target));
 			// if found to be within safe area then return
 			pointWithin = IsPointWithinCollider (worldContainerCollider, target);
 		}
@@ -101,7 +102,25 @@ public class MovePositionWander : PhysicsBase {
 	}
 
 
+	/**
+	 *  Turn transform towards a target a little each frame
+	 */
+	void RotateTowardsTargetOverTime (Vector3 target, float minRotateTime, float maxRotateTime)
+	{
+		// Determine which direction to rotate towards
+		Vector3 targetDirection = target - transform.position;
 
+		// The step size is equal to speed times frame time.
+		float rotateStep = Random.Range (minRotateTime, maxRotateTime) * Time.deltaTime;
 
+		// Rotate the forward vector towards the target direction by one step
+		Vector3 newDirection = Vector3.RotateTowards (transform.forward, targetDirection, rotateStep, 0.0f);
+
+		// Draw a ray pointing at our target in
+		Debug.DrawRay (transform.position, newDirection, Color.red);
+
+		// Calculate a rotation a step closer to the target and applies rotation to this object
+		transform.rotation = Quaternion.LookRotation (newDirection);
+	}
 
 }
